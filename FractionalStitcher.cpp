d2acf417b4ec7c25b8a79ee5e9e270be69f38f28
VasiliiSokolov <vasilysokolov@mail.ru>
2022-10-14 15:05:19 +0300

    Добавьте файлы проекта.

---
+++ FractionalStitcher.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string integer, fractional, result;
+    double transformation;
+    std::cout << "Enter integer part of number: \n";
+    std::cin >> integer;
+    std::cout << "Enter fractional part of number: \n";
+    std::cin >> fractional;
+    result = integer + '.' + fractional;
+    transformation = std::stod(result);
+    std::cout << transformation;
+    
+}
